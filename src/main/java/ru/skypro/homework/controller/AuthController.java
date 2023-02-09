c257e3d5df6db14d6888de16d2d64939306c3398
slavzick <slavzick@mail.ru>
2023-02-09 18:14:47 +0500

    Move methods from LoginApiController and RegisterApiController to AuthController Delete RegisterApi Delete RegisterApiController Delete LoginApi Delete LoginApiController Fix API description for: AuthController, UsersApiController Update openapi.yaml

---
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -0,0 +1,42 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.controller.api.AuthApi;
+import ru.skypro.homework.dto.LoginReqDto;
+import ru.skypro.homework.dto.RegisterReqDto;
+import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.service.AuthService;
+
+import static ru.skypro.homework.dto.Role.USER;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+public class AuthController implements AuthApi {
+
+    private final AuthService authService;
+
+    public ResponseEntity<Object> login(@RequestBody LoginReqDto req) {
+        if (authService.login(req.getUsername(), req.getPassword())) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    public ResponseEntity<Void> register(@RequestBody RegisterReqDto req) {
+        Role role = req.getRole() == null ? USER : req.getRole();
+        if (authService.register(req, role)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+}
--- src/main/java/ru/skypro/homework/dto/NewPasswordDto.java
+++ src/main/java/ru/skypro/homework/dto/NewPasswordDto.java
@@ -1,100 +1,81 @@
 package ru.skypro.homework.dto;
 
 import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
 
-/**
- * NewPassword
- */
 @Validated
-@javax.annotation.Generated(value = "ru.skypro.homeworkcodegen.v3.generators.java.SpringCodegen", date = "2023-02-06T18:24:36.081075022Z[GMT]")
-
-
 public class NewPasswordDto {
-  @JsonProperty("currentPassword")
-  private String currentPassword = null;
+    @JsonProperty("currentPassword")
+    private String currentPassword = null;
 
-  @JsonProperty("newPassword")
-  private String newPassword = null;
+    @JsonProperty("newPassword")
+    private String newPassword = null;
 
-  public NewPasswordDto currentPassword(String currentPassword) {
-    this.currentPassword = currentPassword;
-    return this;
-  }
+    public NewPasswordDto currentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+        return this;
+    }
 
-  /**
-   * Get currentPassword
-   * @return currentPassword
-   **/
-  @Schema(description = "")
-  
     public String getCurrentPassword() {
-    return currentPassword;
-  }
+        return currentPassword;
+    }
 
-  public void setCurrentPassword(String currentPassword) {
-    this.currentPassword = currentPassword;
-  }
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
 
-  public NewPasswordDto newPassword(String newPassword) {
-    this.newPassword = newPassword;
-    return this;
-  }
+    public NewPasswordDto newPassword(String newPassword) {
+        this.newPassword = newPassword;
+        return this;
+    }
 
-  /**
-   * Get newPassword
-   * @return newPassword
-   **/
-  @Schema(description = "")
-  
     public String getNewPassword() {
-    return newPassword;
-  }
-
-  public void setNewPassword(String newPassword) {
-    this.newPassword = newPassword;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
+        return newPassword;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
     }
-    NewPasswordDto newPassword = (NewPasswordDto) o;
-    return Objects.equals(this.currentPassword, newPassword.currentPassword) &&
-        Objects.equals(this.newPassword, newPassword.newPassword);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(currentPassword, newPassword);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class NewPassword {\n");
-    
-    sb.append("    currentPassword: ").append(toIndentedString(currentPassword)).append("\n");
-    sb.append("    newPassword: ").append(toIndentedString(newPassword)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NewPasswordDto newPassword = (NewPasswordDto) o;
+        return Objects.equals(this.currentPassword, newPassword.currentPassword) &&
+                Objects.equals(this.newPassword, newPassword.newPassword);
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currentPassword, newPassword);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class NewPassword {\n");
+
+        sb.append("    currentPassword: ").append(toIndentedString(currentPassword)).append("\n");
+        sb.append("    newPassword: ").append(toIndentedString(newPassword)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
 }
