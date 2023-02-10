496369675d89df954610bf43c2d5925463cb01ee
nadillustrator <nadin2208@gmail.com>
2023-02-10 21:27:39 +0300

    Create entities

---
+++ src/main/java/ru/skypro/homework/entity/Ads.java
@@ -0,0 +1,44 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Ads {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @OneToOne
+    @JoinColumn(name = "id_author")
+    private User author;
+    @Column(name = "price")
+    private Integer price;
+    @Column(name = "title")
+    private String title;
+    @OneToMany(mappedBy="ads", fetch=FetchType.EAGER)
+    private List<Image> images;
+    @Column(name = "description")
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Ads ads = (Ads) o;
+        return id != null && Objects.equals(id, ads.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -0,0 +1,40 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @OneToOne
+    @JoinColumn(name = "id_author")
+    private User author;
+    @Column(name = "text")
+    private String text;
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Comment comment = (Comment) o;
+        return id != null && Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
