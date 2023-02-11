b3ab7fec961a382f4622e2247742e79f8bacc6fd
nadillustrator <nadin2208@gmail.com>
2023-02-11 22:43:59 +0300

    Create liquibase dependencies in pom.xml, structure in resources, migrations for creating tables Some fixes for Entities & Repositories

--- src/main/java/ru/skypro/homework/repository/AvatarRepository.java
+++ src/main/java/ru/skypro/homework/repository/AvatarRepository.java
@@ -2,7 +2,8 @@ package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.skypro.homework.entity.Avatar;
 
 @Repository
-public interface AvatarRepository extends JpaRepository<AvatarRepository, Integer> {
+public interface AvatarRepository extends JpaRepository<Avatar, Integer> {
 }
--- src/main/java/ru/skypro/homework/repository/UsersRepository.java
+++ src/main/java/ru/skypro/homework/repository/UsersRepository.java
@@ -2,7 +2,8 @@ package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.skypro.homework.entity.User;
 
 @Repository
-public interface CityRepository extends JpaRepository<CityRepository, Integer> {
+public interface UsersRepository extends JpaRepository<User, Integer> {
 }
