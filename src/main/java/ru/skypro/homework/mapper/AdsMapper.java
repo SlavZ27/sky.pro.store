61e8ed18cb11f0310fd0c7070f654366f103872d
slavzick <slavzick@mail.ru>
2023-03-14 10:59:31 +0500

    add descriptions to controllers add interfaces of services

--- src/main/java/ru/skypro/homework/mapper/AdsMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdsMapper.java
@@ -17,7 +17,7 @@ public abstract class AdsMapper {
     @Mapping(target = "author", expression = "java(ads.getAuthor().getId())")
     @Mapping(target = "price", source = "ads.price")
     @Mapping(target = "title", source = "ads.title")
-    @Mapping(target = "image", source = "ads")    //is called mapImageToString()
+    @Mapping(target = "image", source = "ads")
     public abstract AdsDto adsToAdsDto(Ads ads);
 
     String mapImageToString(Ads ads) {
